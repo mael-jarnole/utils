264299c1c035b189d80117d06d6c872ec2001da4
Mael Jarnole <mael.jarnole@gmail.com>
2018-02-19 17:05:56 +0100

    Create sort.ts

---
+++ src/sort.ts
@@ -0,0 +1,15 @@
+export function booleanSort(a: boolean, b: boolean, descending: boolean): number {
+  if (descending) {
+    return (a === b) ? 0 : a ? -1 : 1;
+  } else {
+    return (a === b) ? 0 : a ? 1 : -1;
+  }  
+}
+
+export function alphabeticalSort(a: string, b: any, descending: boolean): string {
+  if (descending) {
+    return (a < b) ? 1 : a > b ? -1 : 0;
+  } else {
+    return (a > b) ? 1 : a < b ? -1 : 0;
+  }
+}
